8b580054551cd5a0bfed9cbd3bce786f109298ff
Gleyver Coutinho Castro <gleyver.castro@infracommerce.com.br>
2025-06-16 00:35:39 -0300

    initial commit

--- src/domains/user/tests/UserList.test.tsx
+++ src/domains/user/tests/UserList.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import UserList from '../pages/UserList';
+import { expect, it, describe } from 'vitest';
 
 describe('UserList', () => {
   it('renderiza o título', () => {
     render(<UserList />);
     expect(screen.getByText('Lista de Usuários')).toBeInTheDocument();
   });
-}); 
+});
