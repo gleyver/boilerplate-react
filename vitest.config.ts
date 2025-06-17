14eac0058bf7b0f6d18972fda18072eedd751e66
Gleyver Coutinho Castro <gleyver.castro@tradeupgroup.com>
2025-06-17 09:05:53 -0300

    fix: teste

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +9,9 @@ export default defineConfig({
     setupFiles: './src/setupTests.ts',
     coverage: {
       reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/**', 'src/setupTests.ts'],
     },
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.{idea,git,cache,output,temp}/**'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
-}); 
+})
