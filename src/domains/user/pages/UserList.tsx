c4b0b139b1c09f7f460eaabe399945312488c96c
Gleyver Coutinho Castro <gleyver.castro@infracommerce.com.br>
2025-07-01 21:34:26 -0300

    fix: 🐛 change tailwind

--- src/domains/user/pages/UserList.tsx
+++ src/domains/user/pages/UserList.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import { Button } from '@components/button/Button'
+import React from 'react'
 
 const UserList: React.FC = () => {
   return (
     <div>
-      <h1 className='text-red-500'>
-        Lista de Usuários
-      </h1>
+      <h1 className='text-red-500'>Lista de Usuários</h1>
+      <Button>as</Button>
       {/* Aqui você pode renderizar uma lista de usuários */}
     </div>
-  );
-};
+  )
+}
 
-export default UserList; 
+export default UserList
