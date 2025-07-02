3f1f2f2b0896fdccf5b1d92bac2f3d7b03b0dad5
Gleyver Coutinho Castro <gleyver.castro@infracommerce.com.br>
2025-07-01 22:23:03 -0300

    fix: 🐛 adjust stats

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), visualizer({ open: false }), tailwindcss(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis do .env de acordo com o modo (development, production, etc)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const enableVisualizer = env.VITE_VISUALIZER === 'true'
+
+  return {
+    plugins: [
+      react(),
+      enableVisualizer && visualizer({ open: false }),
+      tailwindcss(),
+      tsconfigPaths(),
+    ].filter(Boolean),
+  }
 })
